3a0f5d328d3ac0a9f23f4a02ff9754712b3fec55
marcos1305 <marcos1305pt@gmail.com>
2018-07-16 10:57:11 -0300

    Construção do layout principal para as outras views

---
+++ resources/views/clientes/baselayout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MindTec</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a href="" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">Cadastrar novo cliente</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+<script src="{{ asset( 'js/app.js') }}"></script>
+</body>
+</html>
